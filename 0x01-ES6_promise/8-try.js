d030d29987bd5f8ab722535e8cacddbd98e5d4d4
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-03 02:27:48 +0100

    update

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,13 @@
 export default function divideFunction(numerator, denominator) {
   return new Promise((resolve, reject) => {
-    if (denominator === 0) {
-      reject(new Error('Cannot divide by zero'));
-    } else {
-      resolve(numerator / denominator);
+    try {
+      if (denominator !== 0) {
+        resolve(numerator / denominator);
+      } else {
+        throw new Error('cannot divide by zero');
+      }
+    } catch (error) {
+      reject(error);
     }
   });
 }
