10f5b4f72015e1ac0bf85df2ee839edb916f2601
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-04 05:41:23 +0100

    task 10 fix

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,14 +1,15 @@
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
+
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string' || typeof motor !== 'string' || typeof color !== 'string') {
-      throw new Error('All car properties must be a string');
-    }
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    return new this.constructor(this[_brand], this[_motor], this[_color]);
   }
 }
