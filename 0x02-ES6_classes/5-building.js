da950447b128b254ecb7461e38a2faf5f1482ea1
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-04 04:16:07 +0100

    updated

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -4,6 +4,12 @@ export default class Building {
       throw new TypeError('sqft must be a number');
     }
     this._sqft = sqft;
+
+    if (new.target !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
   }
 
   get sqft() {
