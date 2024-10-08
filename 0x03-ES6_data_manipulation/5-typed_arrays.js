e14c3542334474dbc6a06ce082d98e4b7e097f7c
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-08 11:40:02 +0100

    task 5 done

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  let view = new DataView(buffer);
+
+  view.setInt8(position, value);
+
+  if (view.getInt8(position) !== value) {
+    throw new Error('Position outside range');
+  }
+  return view;
+}
