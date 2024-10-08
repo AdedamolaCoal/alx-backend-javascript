e03721064e8ff0c8ece4792c591beab07857858f
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-08 03:14:59 +0100

    task 3 alternative approach

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,9 @@
 export default function getStudentIdsSum(idsToSum) {
   const c = 0;
-  const mapIds = idsToSum.map((a) => a.id);
-  const sumIds = mapIds.reduce((a, b) => a + b, c);
+  // const mapIds = idsToSum.map((a) => a.id);
+  // const sumIds = mapIds.reduce((a, b) => a + b, c);
+  console.log('before reducer: ', idsToSum);
+  const sumIds = idsToSum.reduce((a, b) => a + b.id, c);
+  console.log('after reducer: ', sumIds);
   return sumIds;
 }
