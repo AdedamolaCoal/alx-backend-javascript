15e5d67c92b997af3b7860a89a47e19d8e9a8f82
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-10 04:34:59 +0100

    task 3 done

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,13 @@
+import { RowID, RowElement } from "./interface";
+
+export function insertRow(row: RowElement): number {
+	throw new Error("Function not implemented.");
+}
+
+export function updateRow(newRowID: number, updateRow: RowElement) {
+	throw new Error("Function not implemented.");
+}
+
+export function deleteRow(newRowID: number) {
+	throw new Error("Function not implemented.");
+}
