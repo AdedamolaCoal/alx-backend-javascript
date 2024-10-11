89ba261b76b2c4f56e738b14dec7fe744b4270a3
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-12 00:51:25 +0100

    task 10 done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 export default function cleanSet(set, startString) {
-  return Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
-    .join('-');
+  let result = '';
+  set.forEach(value => {
+    if (value.startsWith(startString)) {
+      result += `${value.slice(startString.length)}-`;
+    }
+  });
+  return result.slice(0, -1);
 }
