3c42604edd0a2993eb1cf242233669cda2c552b2
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-10-12 01:03:24 +0100

    update

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,15 +1,10 @@
-const weakMap = new WeakMap();
+export const weakMap = new WeakMap();
 
-function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
-  } else {
-    const count = weakMap.get(endpoint);
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    }
-    weakMap.set(endpoint, count + 1);
+export function queryAPI(endpoint) {
+  const count = weakMap.get(endpoint) || 0;
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
   }
+  weakMap.set(endpoint, count + 1);
 }
-
-export { weakMap, queryAPI };
