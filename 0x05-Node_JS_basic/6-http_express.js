1eda1f36584d86e26d49cee4dae7be2e41b0a161
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-11-26 11:53:08 +0100

    update 6

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -5,4 +5,9 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 })
 
-app.listen(1245);
+app.listen(1245, () => {
+  console.log('Server is running on port 1245');
+});
+
+
+module.exports = app;
